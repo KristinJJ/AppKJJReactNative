888dbc77997e537f7c21bee45f09efaed58ce772
KristinJJ <kristin.jue@gmail.com>
2022-09-25 17:28:04 -0700

    navigation working

--- App.js
+++ App.js
@@ -7,6 +7,7 @@
  */
 
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
 import {Node} from 'react';
 import {
   SafeAreaView,
@@ -14,57 +15,17 @@ import {
   StyleSheet,
   Text,
   useColorScheme,
-  View,
   FlatList,
+  Button,
 } from 'react-native';
-
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
+const Stack = createNativeStackNavigator();
+
+const HomeScreen = ({navigation}) => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <Text style={styles.title}>AppKJJ</Text>
-      <Text style={styles.text}>
-        This is a React Native app for AD 340. Below is a list of some new tv shows and movies.
-      </Text>
+    <>
       <FlatList
         data={[
           {key: 'The Sandman'},
@@ -72,9 +33,35 @@ const App = () => {
           {key: 'Our Great National Parks'},
           {key: 'House of the Dragon'},
         ]}
-        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        renderItem={({item}) => (
+          <Button
+            title={item.key}
+            onPress={() => navigation.navigate('Details', {name: item.key})}
+          />
+        )}
       />
-    </SafeAreaView>
+    </>
+  );
+};
+
+const DetailScreen = ({route}) => {
+  return <Text>This is {route.params.name}'s details</Text>;
+};
+
+const App = () => {
+  return (
+    <>
+      <Text style={styles.title}>AppKJJ</Text>
+      <Text style={styles.text}>
+        This is a React Native app for AD 340. Below is a list of people.
+      </Text>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Details" component={DetailScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 };
 
@@ -90,30 +77,10 @@ const styles = StyleSheet.create({
     fontSize: 24,
     padding: 20,
   },
-  container: {
-    flex: 1,
-    paddingTop: 22,
-  },
   item: {
     padding: 10,
     fontSize: 18,
     height: 44,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 48,
-    fontWeight: 'bold',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
   },
 });
 
